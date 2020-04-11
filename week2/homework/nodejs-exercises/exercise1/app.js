6d7f2ef483df1d65f60f1417424a34d3c8c2c5a3
hettinia <abedhettini@gmail.com>
2020-04-11 23:55:50 +0200

    Finished week-2 homework

---
+++ week2/homework/nodejs-exercises/exercise1/app.js
@@ -0,0 +1,63 @@
+'use strict';
+
+const express = require('express');
+const fs = require("fs");
+const app = new express(); // web server using Express
+app.use(express.json());
+
+
+// Creating new posts
+app.post('/blogs', (req, res) => {
+    //get the tile and content from the request
+    const title = req.body.title;
+    const content = req.body.content;
+    
+    fs.writeFileSync(title, content);
+    res.end('ok')
+});
+
+// Updating existing posts
+app.put('/blogs', (req, res) => {
+    // get the tile and content from the request
+    const title = req.body.title;
+    const content = req.body.content;
+
+    if (fs.existsSync(title)) {
+        fs.writeFileSync(title, content);
+        res.end('ok')
+      }
+      else {
+        res.end('post does not exist');
+      }
+});
+
+// Deleting posts
+app.delete('/blogs/:title', (req, res) => {
+    //get the tilte from the url parameters
+    const title = req.params.title;
+    
+    if (fs.existsSync(title)) {
+        fs.unlinkSync(title);
+        res.end('ok')
+      }
+      else {
+        res.end('post does not exist');
+      }
+});
+
+// Reading posts
+app.get('/blogs/:title', (req, res) => {
+    //get the tilte from the url parameters
+    const title = req.params.title;
+    
+    if (fs.existsSync(title)) {
+        res.sendfile(title);
+      }
+      else {
+        res.end('post does not exist');
+      }
+});
+
+//the server listens on port 3000
+app.listen(3000);
+
